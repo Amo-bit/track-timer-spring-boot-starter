4d0331d94dccca7672f909815a791c50bcb04fef
Ilmir <ilmir302@yandex.ru>
2024-04-10 20:53:37 +0500

    init

---
+++ src/main/java/com/islamov/tracktimer/tracktimerspringbootstarter/TrackTimeSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.islamov.tracktimer.tracktimerspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackTimeSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackTimeSpringBootStarterApplication.class, args);
+    }
+
+}
